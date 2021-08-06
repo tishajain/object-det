86555f23152d1b337cfa6c1966a38ad9ceb8817b
tishajain <74290165+tishajain@users.noreply.github.com>
2021-08-06 17:38:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 img = "";
+status = "";
 
 function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
 function preload()
@@ -16,7 +19,29 @@ function draw()
     image(img, 0, 0, 640, 420);
     fill("#FF0000")
     text("dog", 45, 75);
-    nofill();
+    noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
 }
